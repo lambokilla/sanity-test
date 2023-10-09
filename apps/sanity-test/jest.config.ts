198dd6bcb4cf9f618092bdd9293d87ff4e21c517
Ben Scheib <lambo172@gmail.com>
2023-10-09 12:57:19 -0400

    Initial commit

---
+++ apps/sanity-test/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'sanity-test',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/sanity-test',
+};
